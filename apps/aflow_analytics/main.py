7283109201798c73ac65328bd6fec7a3a7b078c8
CHEN WENLONG <chenwenlongofficial@gmail.com>
2024-12-30 18:10:41 +0200

    find your flow analytics for emil.

---
+++ apps/aflow_analytics/main.py
@@ -0,0 +1,5 @@
+from utils import read_csv
+
+df = read_csv("./apps/aflow-analytics/assets/export-tickuserstate-20241230_121834.csv")
+
+print(df)
---
+++ apps/aflow_analytics/utils/__init__.py
@@ -0,0 +1,3 @@
+from .file_manager import read_csv
+
+__all__ = ["read_csv"]
